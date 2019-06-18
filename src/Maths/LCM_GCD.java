1ac25959cf29d8e8f8987f02c8b91c1e54c2b006
abhijeet1069 <abhijeet1069@gmail.com>
2019-06-18 18:52:05 +0530

    Second Commit

--- src/Maths/LCM_GCD.java
+++ src/Maths/LCM_GCD.java
@@ -5,9 +5,13 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class LCM_GCD {
+
+    public static int square(int x){
+        return x*x;
+    }
     public static void main(String[] args) throws IOException{
         BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
         String str = in.readLine();
-        System.out.println(str);
+        System.out.println(square(Integer.parseInt(str)));
     }
 }
