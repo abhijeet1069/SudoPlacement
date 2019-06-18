afef5bfd3f61521c409fba1837236d7f5f8f1496
abhijeet1069 <abhijeet1069@gmail.com>
2019-06-18 20:51:46 +0530

    Accepted in 0.5 seconds

--- src/Maths/LCM_GCD.java
+++ src/Maths/LCM_GCD.java
@@ -1,17 +1,31 @@
 package Maths;
 
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class LCM_GCD {
-
-    public static int square(int x){
-        return x*x;
+class Solver{
+    public int GCD(int a, int b){
+        if (b == 0) {
+            return a;
+        }
+        return GCD(b , a % b);
     }
-    public static void main(String[] args) throws IOException{
-        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
-        String str = in.readLine();
-        System.out.println(square(Integer.parseInt(str)));
+}
+
+public class LCM_GCD {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int test = Integer.parseInt(br.readLine());
+        Solver solve = new Solver();
+        for (int i = 0; i < test; i++){
+            String line = br.readLine();
+            String[] strs = line.trim().split("\\s+");
+            int a = Integer.parseInt(strs[0]);
+            int b = Integer.parseInt(strs[1]);
+            int gcd = solve.GCD(a,b);
+            System.out.println((a*b)/gcd+ " "+gcd);
+        }
     }
 }
