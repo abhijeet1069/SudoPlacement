6243f511d250f95e8b133fde2e97a93b70e5dabe
abhijeet1069 <abhijeet1069@gmail.com>
2019-06-18 18:44:55 +0530

    First Commit

---
+++ src/Maths/LCM_GCD.java
@@ -0,0 +1,13 @@
+package Maths;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class LCM_GCD {
+    public static void main(String[] args) throws IOException{
+        BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+        String str = in.readLine();
+        System.out.println(str);
+    }
+}
