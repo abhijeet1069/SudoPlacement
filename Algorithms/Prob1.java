485d170e5936e48e3693084d545a93afcfc41d0c
abhijeet1069 <46062413+abhijeet1069@users.noreply.github.com>
2019-07-03 16:50:42 +0530

    3n+1, Dynamic Programming

---
+++ Algorithms/Prob1.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
+public class Prob1 {
+    HashMap<Integer,Integer> numCycle = new HashMap<>();
+
+    public int countCycle(int n, int ctr){
+
+        if(numCycle.containsKey(n))
+            return ctr;
+
+        else{
+            if(n==1) {
+                numCycle.put(n, ctr);
+                return ctr;
+            }
+            else if(n%2 == 0)
+               return countCycle(n/2, ++ctr);
+            else
+               return countCycle(3*n+1, ++ctr);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().trim().split("\\s+");
+        Prob1 obj = new Prob1();
+        int max = 0;
+        for (int i = Integer.parseInt(input[0]); i <= Integer.parseInt(input[1]); i++) {
+            int temp = obj.countCycle(i,1);
+            if( temp > max)
+                max = temp;
+        }
+        System.out.println(input[0]+" "+input[1]+" "+max);
+    }
+}
