212356f4b4c4e449baa8169aff1ce4a051b3997b
abhijeet1069 <46062413+abhijeet1069@users.noreply.github.com>
2019-08-08 18:38:42 +0530

    Horner Rule
    Geeks for Geeks version

--- Algorithms/HornerRule.java
+++ Algorithms/HornerRule.java
@@ -1,16 +1,16 @@
 public class HornerRule {
 
     public static int horner(int x, int[] coeff){
-        int res = 0;
-        for(int i = coeff.length-1; i >= 0; i--)
-            res = coeff[i] + x*res;
+        int res = coeff[0];
+        for(int i = 1; i < coeff.length; i++)
+            res = x*res + coeff[i];
         return res;
     }
 
     public static void main(String[] args) {
-        /*(x+1)^3 = x^3 + 3*x^2 + 3*x + 1
-           Here,  (4+1)^3 */
-        int[] coeff = {1,3,3,1};
+        /*(x+1)^3 = 1*x^3 + 3*x^2 + 3*x^1 + 1*x^0  Here,  (4+1)^3 */
+        
+        int[] coeff = {1,3,3,1}; //written in descending power
         System.out.print(horner(4,coeff)); //o/p = 125
     }
 }
