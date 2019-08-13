098c9c461625a949868b69c1de744dae7c2f1653
abhijeet1069 <46062413+abhijeet1069@users.noreply.github.com>
2019-08-13 19:51:40 +0530

    2DMatrix Addition

--- Algorithms/MatrixAddition.java
+++ Algorithms/MatrixAddition.java
@@ -1,6 +1,6 @@
 class MatrixAddition{
     public static void main(String[] args) {
-        int[][] a = {{7, 7, 3}, {2, 5, 12}};
+        int[][] a = {{7, 7, 3}, {2, 5, 12}}; //2x3 matrix (a.length= row= 2, a[0].length= column= 3)
         int[][] b = {{4, 2, 5}, {6, 4, 45}};
         int[][] c = new int[a.length][a[0].length];
         if (a[0].length == b[0].length && a.length == b.length) { //Both matrices should have same row and column
